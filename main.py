8d27d364203840d876a30709f4c2e70d4b7a1583
nakamura196 <na.kamura.1263@gmail.com>
2021-03-11 08:13:16 +0900

    add image and metadata csv

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from PIL import Image
+import argparse    # 1. argparseをインポート
+import glob
+import os
 
-file_path = "input/0001-01.jpeg"
+parser = argparse.ArgumentParser()    # 2. パーサを作る
+
+# 3. parser.add_argumentで受け取る引数を追加していく
+parser.add_argument('path', help='入力フォルダへのパス')    # 必須の引数を追加
+
+args = parser.parse_args()    # 4. 引数を解析
+
+files = []
+
+suffixes = ["JPG", "png"]
+
+for suffix in suffixes:
+    files.extend(glob.glob(args.path+"/**/*.{}".format(suffix), recursive=True))
+
+    
 
 # Orientation タグ値にしたがった処理
 # PIL における Rotate の角度は反時計回りが正
@@ -15,10 +32,22 @@ convert_image = {
     8: lambda img: img.transpose(Image.ROTATE_90),                                    # 反時計回りに90度回転
 }
 
-img = Image.open(file_path)
-exif = img._getexif()
-orientation = exif.get(0x112, 1)
+for file in files:
 
-new_img = convert_image[orientation](img)
+    img = Image.open(file)
+    exif = img._getexif()
 
-new_img.save(file_path.replace("input/", "output/"))
+    if exif:
+
+        orientation = exif.get(0x112, 1)
+
+        new_img = convert_image[orientation](img)
+
+    else:
+        new_img = img
+
+    output_path = file.replace("input/", "output/")
+    dir_path = os.path.dirname(output_path)
+    os.makedirs(dir_path, exist_ok=True)
+
+    new_img.save(output_path)
